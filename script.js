68314bc073cf8b8dfb5d1a64dba137b0fbd932a6
jonathan <jonathanodongtho02@gmail.com>
2024-02-26 09:55:41 +0300

    changed the loader positioning

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.onreadystatechange = function () {
     var loader = document.querySelector(".loader");
     if (document.readyState !== "complete") {
-        loader.style.display = "initial";
+        loader.style.display = "flex";
     }
     else {
         loader.style.display = "none";
